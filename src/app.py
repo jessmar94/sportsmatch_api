a29caa27644ac7f77a8f2396020d6459fb0dc2e0
Duncan Skinner <duncanskinner94@gmail.com>
2019-11-26 16:41:25 +0000

    do not use

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 from .config import app_config
 from .models import db, bcrypt
@@ -26,5 +26,19 @@ def create_app(env_name):
     example endpoint
     """
     return 'Congratulations! Your first endpoint is workin'
+
+  @app.route('/games', methods=['POST'])
+  def add_game_request():
+    organiser_id = request.json['organiser_id']
+    opponent_id = request.json['opponent_id']
+    game_date = request.json['game_date']
+    game_time = request.json['game_time']
+
+    new_game = GameModel(organiser_id, opponent_id, game_date, game_time)
+
+    db.session.add(new_game)
+    db.session.commit()
+
+    return product_schema.jsonify(new_product)
 
   return app
