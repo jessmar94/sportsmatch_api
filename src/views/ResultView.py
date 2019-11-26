541fdab2fbbd6822a97de2fc260d1f01d1ea200c
Jessica Marais <jessica_marais@hotmail.com>
2019-11-26 14:14:24 +0000

    Update result view

--- src/views/ResultView.py
+++ src/views/ResultView.py
@@ -1,5 +1,6 @@
 from flask import request, json, Response, Blueprint
 from ..models.ResultModel import ResultModel, ResultSchema
+from ..models.GameModel import GameModel, GameSchema
 
 result_api = Blueprint('results', __name__)
 result_schema = ResultSchema()
@@ -7,8 +8,8 @@ result_schema = ResultSchema()
 @result_api.route('/', methods=['GET'])
 def get_all():
     results = ResultModel.get_all_results()
-    ser_results = result_schema.dump(results, many=True)
-    return custom_response(ser_results, 200)
+    data = result_schema.dump(results, many=True)
+    return custom_response(data, 200)
 
 def custom_response(res, status_code):
     """
