2c843b50c711aca49aaea0e621d871e4397af19a
Jessica Marais <jessica_marais@hotmail.com>
2019-11-26 17:00:49 +0000

    Result can be created and stored

--- src/views/ResultView.py
+++ src/views/ResultView.py
@@ -2,7 +2,7 @@ from flask import request, json, Response, Blueprint
 from ..models.ResultModel import ResultModel, ResultSchema
 from ..models.GameModel import GameModel, GameSchema
 
-result_api = Blueprint('result_api', __name__)
+result_api = Blueprint('results', __name__)
 result_schema = ResultSchema()
 
 @result_api.route('/', methods=['GET'])
@@ -12,6 +12,20 @@ def get_all():
     results = [*host, *guest]
     data = result_schema.dump(results, many=True)
     return custom_response(data, 200)
+
+@result_api.route('/', methods=['POST'])
+def create():
+      """
+      Create Result Function
+      """
+      # request similar to http
+      req_data = request.get_json()
+      # load in format of resultschema
+      data = result_schema.load(req_data)
+
+      result = ResultModel(data)
+      result.save()
+      return custom_response(data, 201)
 
 def custom_response(res, status_code):
     """
