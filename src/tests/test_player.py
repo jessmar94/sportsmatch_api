5cbc61991ba8b44f74473163201936498859690e
Mezela <p_mezue@hotmail.com>
2019-11-27 16:10:48 +0000

    Add test to check player can't see their own name in the view where they see all other users

--- src/tests/test_player.py
+++ src/tests/test_player.py
@@ -137,13 +137,35 @@ class PlayersTest(unittest.TestCase):
     res = self.client().get('api/v1/players/', headers={'Content-Type': 'application/json', 'api-token': api_token})
     json_data = json.loads(res.data)
     self.assertEqual(res.status_code, 200)
-    print("====================")
-    print(json_data[0])
-    abilities = []
+    
     for item in json_data:
-      abilities.append(item['ability'])
-      return abilities
-    self.assertEqual(abilities != 'Beginner')
+      if (item['ability']) != 'Beginner':
+        result = "Error!"
+      else:
+        result = "No error"
+
+    self.assertEqual(result, "No error")
+
+  def test_player_cannot_see_their_profile_among_all_players(self):
+    player1 = {
+      "first_name": "Pam",
+      "last_name": "M",
+      "email": "pam@test.com",
+      "password": "password",
+      "gender": "F",
+      "dob": "1990-01-01",
+      "ability": "Beginner"
+    }
+    res = self.client().post('api/v1/players/new', headers={'Content-Type': 'application/json'}, data=json.dumps(player1))
+    self.assertEqual(res.status_code, 201)
+    res = self.client().post('api/v1/players/new', headers={'Content-Type': 'application/json'}, data=json.dumps(self.player))
+    self.assertEqual(res.status_code, 201)
+    api_token = json.loads(res.data).get('jwt_token')
+    res = self.client().get('api/v1/players/', headers={'Content-Type': 'application/json', 'api-token': api_token})
+    json_data = json.loads(res.data)
+    self.assertEqual(res.status_code, 200)
+    self.assertEqual(json_data[0].get('first_name'), 'Pam')
+    self.assertNotEqual(json_data[0].get('first_name'), 'Dom')
 
   def test_player_can_update_their_own_profile(self):
     updated_player = {
