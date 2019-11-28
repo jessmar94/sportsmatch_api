a0f8b9036f8f5e2205a6edd961264f3759e01477
Jessica Marais <jessica_marais@hotmail.com>
2019-11-28 14:15:24 +0000

    gamemodel update

--- src/models/GameModel.py
+++ src/models/GameModel.py
@@ -20,9 +20,15 @@ class GameModel(db.Model): # GameModel class inherits from db.Model
   game_time = db.Column(db.Time, nullable=False)
   created_at = db.Column(db.DateTime)
   modified_at = db.Column(db.DateTime)
+  # new attempt
+  organiser = db.relationship("PlayerModel", primaryjoin = "GameModel.organiser_id == PlayerModel.id", backref="organiser")
+  opponent = db.relationship("PlayerModel", primaryjoin = "GameModel.opponent_id == PlayerModel.id", backref="opponent")
+
+  # many:many way
+    # result = db.relationship("ResultModel", uselist=False, back_populates="game")
+  # old way
   # organiser = db.relationship("PlayerModel", foreign_keys=[organiser_id])
   # opponent = db.relationship("PlayerModel", foreign_keys=[opponent_id])
-  result = db.relationship("ResultModel", uselist=False, back_populates="game")
   # game = db.relationship("ResultModel", backref="gameModel", lazy=True)
 
   # class constructor
