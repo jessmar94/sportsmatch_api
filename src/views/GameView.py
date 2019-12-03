25e5107e5683753999f7ee4fde007db470585c75
Jessica Marais <jessica_marais@hotmail.com>
2019-12-03 09:26:03 +0000

    Update games get methods to show games and results data

--- src/views/GameView.py
+++ src/views/GameView.py
@@ -2,10 +2,12 @@ from flask import request, g, Blueprint, json, Response
 from ..shared.Authentication import Auth
 from ..models.GameModel import GameModel, GameSchema
 from ..models.PlayerModel import PlayerModel, PlayerSchema
+from ..models.ResultModel import ResultModel, ResultSchema
 
 game_api = Blueprint('game_api', __name__)
 game_schema = GameSchema()
 player_schema = PlayerSchema()
+result_schema = ResultSchema()
 
 def custom_response(res, status_code):
   """
@@ -52,16 +54,42 @@ def get_all_opponent_games():
     """
     Get Past Confirmed Games related to an opponent
     """
-    user_id = Auth.current_user_id()
-    games = GameModel.get_game_by_opp_id(user_id)
-    data = game_schema.dump(games, many=True)
+    current_user_id = Auth.current_user_id()
+    guest = GameModel.get_game_by_opp_id(current_user_id)
+    games = [*guest]
     results = []
-    for game in data:
-      if game['organiser_id'] == user_id:
-        results.append({**game, **PlayerModel.get_opponent_info(game['opponent_id'])})
-      elif game['opponent_id'] == user_id:
-        results.append({**game, **PlayerModel.get_opponent_info(game['organiser_id'])})
-    return custom_response(results, 200)
+    game_results = []
+    for game in games:
+        result = ResultModel.get_all_results(game.id)
+        game = GameModel.get_games_by_id(game.id)
+        formatted_result = result_schema.dump(result, many=True)
+        formatted_game_info = game_schema.dump(game, many=True)
+        # if not formatted_result:
+        #     continue
+        #
+        # if not formatted_game_info:
+        #     continue
+
+        if len(formatted_result) == 0:
+            game_results.append(formatted_game_info[0])
+            print("RESULTS")
+            print(game_results)
+        elif len(formatted_result) != 0:
+            results.append(formatted_result[0])
+            partial_results = results[0]
+            results.append(formatted_game_info[0])
+            partial_games = results[1]
+            final_results = {**partial_results, **partial_games}
+            game_results.append(final_results)
+    return custom_response(game_results, 200)
+    # data = game_schema.dump(games, many=True)
+    # results = []
+    # for game in data:
+    #   if game['organiser_id'] == user_id:
+    #     results.append({**game, **PlayerModel.get_opponent_info(game['opponent_id'])})
+    #   elif game['opponent_id'] == user_id:
+    #     results.append({**game, **PlayerModel.get_opponent_info(game['organiser_id'])})
+    # return custom_response(results, 200)
 
 @game_api.route('/organiser', methods=['GET'])
 @Auth.auth_required
@@ -69,16 +97,48 @@ def get_all_organiser_games():
     """
     Get Past Confirmed Games related to an opponent
     """
-    user_id = Auth.current_user_id()
-    games = GameModel.get_game_by_org_id(user_id)
-    data = game_schema.dump(games, many=True)
+    current_user_id = Auth.current_user_id()
+    host = GameModel.get_game_by_org_id(current_user_id)
+    games = [*host]
     results = []
-    for game in data:
-      if game['organiser_id'] == user_id:
-        results.append({**game, **PlayerModel.get_opponent_info(game['opponent_id'])})
-      elif game['opponent_id'] == user_id:
-        results.append({**game, **PlayerModel.get_opponent_info(game['organiser_id'])})
-    return custom_response(results, 200)
+    game_results = []
+    for game in games:
+        result = ResultModel.get_all_results(game.id)
+        game = GameModel.get_games_by_id(game.id)
+        formatted_result = result_schema.dump(result, many=True)
+        formatted_game_info = game_schema.dump(game, many=True)
+        if len(formatted_result) == 0:
+            game_results.append(formatted_game_info[0])
+            print("RESULTS")
+            print(game_results)
+            # partial_games = results[0]
+            # print("RESULTS")
+            # print(results)
+            # final_results = {**partial_games}
+            # game_results.append(results)
+        elif len(formatted_result) != 0:
+            results.append(formatted_result[0])
+            partial_results = results[0]
+            results.append(formatted_game_info[0])
+            partial_games = results[1]
+            final_results = {**partial_results, **partial_games}
+            game_results.append(final_results)
+        # if not formatted_result:
+        #     continue
+        #
+        # if not formatted_game_info:
+        #     continue
+    return custom_response(game_results, 200)
+    # user_id = Auth.current_user_id()
+    # games = GameModel.get_game_by_org_id(user_id)
+    # data = game_schema.dump(games, many=True)
+    # results = []
+    # for game in data:
+    #   if game['organiser_id'] == user_id:
+    #     results.append({**game, **PlayerModel.get_opponent_info(game['opponent_id'])})
+    #   elif game['opponent_id'] == user_id:
+    #     results.append({**game, **PlayerModel.get_opponent_info(game['organiser_id'])})
+    # return custom_response(results, 200)
 
 @game_api.route('/', methods=['POST'])
 @Auth.auth_required
